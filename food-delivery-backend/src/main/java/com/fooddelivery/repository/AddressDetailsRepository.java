aad1c12022c02eee76c8cc8275ebf30d4d861e7e
Ankul Singh Thakur <ankulsingh221@gmail.com>
2025-08-06 11:19:38 +0530

    Delete food-delivery-backend directory

--- food-delivery-backend/src/main/java/com/fooddelivery/repository/AddressDetailsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fooddelivery.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.fooddelivery.entity.AddressDetails;
-
-public interface AddressDetailsRepository extends CrudRepository<AddressDetails, Long>{
-
-}
--- food-delivery-backend/src/main/java/com/fooddelivery/repository/DeliveryDetailsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fooddelivery.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.fooddelivery.entity.DeliveryDetails;
-
-public interface DeliveryDetailsRepository extends CrudRepository<DeliveryDetails, Long> {
-
-}
