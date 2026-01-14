9d6fefc177c3da3b04051452cf8a66a7aa379847
Sharvari <sharvarikarnase4@gmail.com>
2026-01-14 20:00:28 +0530

    Backend Added

---
+++ food-delivery-backend/src/main/java/com/quickbite/food_delivery_backend/FoodDeliveryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.quickbite.food_delivery_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDeliveryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodDeliveryBackendApplication.class, args);
+	}
+
+}
